910717cc2f3318b17a8f49c061b748630a330dc2
PauloAugusto <paulo.augustog@outlook.com>
2026-02-05 15:18:39 -0400

    Adiciona cenario de login com sucesso - Teste de API

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,4 +1,4 @@
-describe('Cadastro de usuario', () => {
+describe('API - Cadastro de usuario', () => {
 
   it('Deve criar um usuário com sucesso', () => {
     const email = `paulo_${Date.now()}@qa.com`
@@ -19,3 +19,42 @@ describe('Cadastro de usuario', () => {
   })
 
 })
+
+describe('API - Login de usuario', () => {
+
+  it('Deve realizar login com sucesso', () => {
+    const email = `paulo_${Date.now()}@qa.com`
+    const password = '123456'
+
+    // Cadastro do usuário
+    cy.request({
+      method: 'POST',
+      url: 'https://serverest.dev/usuarios',
+      body: {
+        nome: 'Paulo Augusto',
+        email: email,
+        password: password,
+        administrador: 'true'
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(201)
+      expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+    })
+
+    // Login do usuário
+    cy.request({
+      method: 'POST',
+      url: 'https://serverest.dev/login',
+      body: {
+        email: email,
+        password: password
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.message).to.eq('Login realizado com sucesso')
+      expect(response.body).to.have.property('authorization')
+    })
+  })
+
+})
+
