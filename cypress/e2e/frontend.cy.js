06c20fb5d262672034eca8552239b85a402484b5
PauloAugusto <paulo.augustog@outlook.com>
2026-02-05 13:40:10 -0400

    Adiciona cenario de cadastro com email ja existente

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -1,22 +1,46 @@
-const { describe } = require("mocha")
-
 describe('Cadastro de usuário', () => {
   it('Deve cadastrar um novo usuário com sucesso', () => {
-    //Conecta no site
+    const email = `paulo_${Date.now()}@qa.com`
+
+    // Conecta no site
     cy.visit('https://front.serverest.dev')
 
-    //clica no botão cadastre-se
+    // Clica no botão cadastre-se
     cy.get('[data-testid="cadastrar"]').click()
 
-    //preenche os dados de cadastro
+    // Preenche os dados de cadastro
     cy.get('[data-testid="nome"]').type('Paulo Augusto')
-    cy.get('[data-testid="email"]').type('paulo@qa.com')
+    cy.get('[data-testid="email"]').type(email)
     cy.get('[data-testid="password"]').type('123456')
     cy.get('[data-testid="cadastrar"]').click()
 
-
-    //valida se o cadastro foi criado com sucesso e redirecionado para a página home
+    // Valida se o cadastro foi criado com sucesso
     cy.url().should('include', '/home')
   })
 })
 
+describe('Login', () => {
+  it('Deve realizar login com usuário válido', () => {
+    const email = `paulo_${Date.now()}@qa.com`
+    const senha = '123456'
+
+    // Cria o usuário antes de logar
+    cy.visit('https://front.serverest.dev')
+    cy.get('[data-testid="cadastrar"]').click()
+    cy.get('[data-testid="nome"]').type('Paulo Augusto')
+    cy.get('[data-testid="email"]').type(email)
+    cy.get('[data-testid="password"]').type(senha)
+    cy.get('[data-testid="cadastrar"]').click()
+
+    //realizar logout pra testar login   
+    cy.get('[data-testid="logout"]').click();
+    
+    // Realiza login
+    cy.get('[data-testid="email"]').type(email)
+    cy.get('[data-testid="senha"]').type(senha)
+    cy.get('[data-testid="entrar"]').click()
+
+    // Valida se login foi realizado com sucesso
+    cy.url().should('include', '/home')    
+  })
+})
